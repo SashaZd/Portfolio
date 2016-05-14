ce00dae95d92b58a500f62859ad404703977980a
Sasha <sasha.azad@gatech.edu>
2016-05-14 11:34:47 -0400

    extracting wp

--- projects/wp-content/themes/gk-portfolio/customizer/front-end-js.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function portfolio_customize_preview_js($wp_customize) {
-	wp_enqueue_script('customizer-front-end', get_template_directory_uri() . '/js/customize-preview.js', array('customize-preview'));
-}
-
-add_action('customize_preview_init', 'portfolio_customize_preview_js');
-
-// EOF
